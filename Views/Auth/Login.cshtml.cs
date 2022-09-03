b9723fa57ec19262c36a3a34b37a35e56e9c2264
LUIS FILLIPE VENTURINI QUINTINO <luisvq12333@gmail.com>
2022-09-02 22:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ Views/Auth/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PodraoFoodsWeb.Views.Auth
+{
+    public class LginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
